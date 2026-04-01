c1b3ac2e53473cc067db430c38693a0f429163f6
axelbaltar <axelbaltar@gmail.com>
2026-04-01 15:11:08 -0300

    Add files via upload

--- decrypt.c
+++ decrypt.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
+    unsigned char c;
+    int contador = 0;
 
-    // Termina la ejecución del programa.
+    while (read(STDIN_FILENO, &c, 1) > 0)
+    {
+        contador++;
+        if (contador == 8)
+        {
+            write(STDOUT_FILENO, &c, 1);
+            contador = 0;
+        }
+    }
+
     exit(EXIT_SUCCESS);
 }
