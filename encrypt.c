5c509980c1a90bbb8af32265425316c58aee68ca
axelbaltar <axelbaltar@gmail.com>
2026-04-01 15:09:55 -0300

    Add files via upload

--- encrypt.c
+++ encrypt.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
+    srand(time(NULL));
+    unsigned char c;
 
-    // Termina la ejecución del programa.
+    while (read(STDIN_FILENO, &c, 1) > 0)
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            unsigned char b = rand();
+            write(STDOUT_FILENO, &b, 1);
+        }
+        write(STDOUT_FILENO, &c, 1);
+        
+    }
+
     exit(EXIT_SUCCESS);
 }
+
