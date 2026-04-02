12c9a12e4c13d4d60b1e8e30f554e98927df5200
axelbaltar <axelbaltar@gmail.com>
2026-04-02 12:50:39 -0300

    Add files via upload

--- decrypt.c
+++ decrypt.c
@@ -2,20 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-    unsigned char c;
-    int contador = 0;
+int main(int argc, char *argv[]) {
+  unsigned char letra;
+  int contador = 0;
 
-    while (read(STDIN_FILENO, &c, 1) > 0)
-    {
-        contador++;
-        if (contador == 8)
-        {
-            write(STDOUT_FILENO, &c, 1);
-            contador = 0;
-        }
+  while (read(STDIN_FILENO, &letra, 1) > 0) {
+    contador++;
+    if (contador == 8) {
+      write(STDOUT_FILENO, &letra, 1);
+      contador = 0;
     }
+  }
 
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
--- encrypt.c
+++ encrypt.c
@@ -3,22 +3,39 @@
 #include <time.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-    srand(time(NULL));
-    unsigned char c;
+int main(int argc, char *argv[]) {
+  srand(time(NULL));
+  unsigned char letra;
 
-    while (read(STDIN_FILENO, &c, 1) > 0)
-    {
-        for (int i = 0; i < 7; i++)
-        {
-            unsigned char b = rand();
-            write(STDOUT_FILENO, &b, 1);
+  if (argc > 1) {
+    for (int j = 0; argv[1][j] != '\0'; j++) {
+      letra = (unsigned char)argv[1][j];
+      for (int i = 0; i < 7; i++) {
+        unsigned char basura = (unsigned char)rand();
+        if (write(1, &basura, 1) == -1) {
+          perror("Error");
+          return 1;
         }
-        write(STDOUT_FILENO, &c, 1);
-        
+      }
+      if (write(1, &letra, 1) == -1) {
+        perror("Error");
+        return 1;
+      }
     }
-
-    exit(EXIT_SUCCESS);
+  } else {
+    while (read(0, &letra, 1) > 0) {
+      for (int i = 0; i < 7; i++) {
+        unsigned char basura = (unsigned char)rand();
+        if (write(1, &basura, 1) == -1) {
+          perror("Error");
+          return 1;
+        }
+      }
+      if (write(1, &letra, 1) == -1) {
+        perror("Error");
+        return 1;
+      }
+    }
+  }
+  return 0;
 }
-
